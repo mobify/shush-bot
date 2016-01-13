6acbdc967b01cfd7321acbba2af77c69a094e79b
fractaltheory <michael@mobify.com>
2016-01-13 14:15:57 -0800

    set config function, add threshold range input

--- client/webapp.js
+++ client/webapp.js
@@ -6,28 +6,68 @@
 
     var $list = $('#bots');
 
-    var setConfiguration = function() {
+    var setConfiguration = function($bot) {
+        var inputThreshold = $bot.find('[data-type="threshold"]').text();
+        var speakerVolume = $bot.find('[data-type="volume"]').text();
+        var state = $bot.find('[data-type="state"] option:selected').val();
 
+        $.ajax({
+            url: 'http://localhost:3000',
+            method: 'POST',
+            data: {
+                inputThreshold: '',
+                speakerVolume: '',
+                state: ''
+            }
+        });
     };
 
     var renderList = function(data) {
         var $template = $('#bots-template');
 
-        var $clone = $($template[0].content.querySelectorAll('li')).clone();
+        var $liClone = $($template[0].content.querySelectorAll('li')).clone();
+        // var $selectClone = $($template[0].content.querySelectorAll('select')).clone();
+        var $rangeClone = $($template[0].content.querySelectorAll('.range')).clone();
 
-        var $name = $clone.find('.bot-name');
-        var $info = $clone.find('.info');
-
+        // Bot name
+        var $name = $liClone.find('.bot-name');
         $name.text(data.name);
-        $info.find('.type').text(data.type);
-        $info.find('.data').text(data.data);
 
-        if (READ_ONLY.indexOf(data.type) !== -1) {
-            $info.find('.data').attr('readonly', 'readonly');
-            $info.find('.submit').attr('disabled', 'disabled');
+        // Configuration data
+        for (var key in data.configuration) {
+            if (data.configuration.hasOwnProperty(key)) {
+                var $rowClone = $($template[0].content.querySelectorAll('.row')).clone();
+
+                var $type = $rowClone.find('.type');
+                var $value = $rowClone.find('.value');
+
+                var value = data.configuration[key];
+                $type.text(key);
+
+                if (key === 'threshold') {
+                    $rangeClone.val(value);
+                    $value.replaceWith($rangeClone);
+                } else {
+                    $value.text(value);
+                }
+
+                // if (key === 'state') {
+                //     $selectClone.find('option').eq(parseInt(value, 10)).attr('selected', 'selected');
+                //     $value.replaceWith($selectClone);
+                // } else {
+                //     $value.text(value);
+                // }
+
+                if (READ_ONLY.indexOf(key) !== -1) {
+                    $value.attr('readonly', 'readonly');
+                    $rowClone.find('.submit').attr('disabled', 'disabled');
+                }
+
+                $liClone.find('.info').append($rowClone);
+            }
         }
 
-        $list.append($clone);
+        $list.append($liClone);
     };
 
     var getBots = function() {
@@ -37,6 +77,7 @@
             $.ajax({
                 // url: SERVER + '/',
                 url: 'http://localhost:3000',
+                method: 'GET',
                 complete: function(xhr, status) {
                     $list.empty();
                     // var res = JSON.parse(xhr.responseText);
@@ -45,12 +86,18 @@
                         bots: [
                             {
                                 name: 'bot1',
-                                type: 'timestamp',
-                                info: '13135823058'
+                                configuration: {
+                                    timestamp: '13135823058',
+                                    threshold: '0.8'
+                                }
                             },
                             {
-                                name: 'bot2'
-                            }
+                                name: 'bot2',
+                                configuration: {
+                                    timestamp: '13135823058',
+                                    state: '1'
+                                }
+                            },
                         ]
                     };
 
@@ -64,6 +111,11 @@
 
     var bindListeners = function() {
         getBots();
+
+        $list.on('click', '.submit', function(evt) {
+            var $bot = $(evt.target).parents('.bot');
+            setConfiguration($bot);
+        });
     };
 
     var main = function() {
