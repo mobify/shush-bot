f3a5245ebc0fe97b42ace8b6f890d51ac9ff6da0
Ben Chypak <bchypak@mobify.com>
2016-01-13 11:51:00 -0800

    Added playback on threshold

--- app/capture.py
+++ app/capture.py
@@ -1,6 +1,34 @@
 import numpy
 import pyaudio
 import analyse
+import wave
+import time
+
+shushing = False
+loudness = -40
+
+def shush():
+    shushing = True
+    chunk = 1024
+    wf = wave.open('app/sound-samples/shush.wav', 'rb')
+    p = pyaudio.PyAudio()
+
+    stream = p.open(
+        format = p.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        rate = wf.getframerate(),
+        output = True)
+
+    data = wf.readframes(chunk)
+
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    shushing = False
 
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
@@ -16,9 +44,21 @@ stream = pyaud.open(
 )
 
 while True:
-    # Read raw microphone data
-    rawsamps = stream.read(1024)
-    # Convert raw data to NumPy array
-    samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-    # Show the volume and pitch
-    print analyse.loudness(samps), analyse.musical_detect_pitch(samps)
+
+    if not shushing:
+
+        # Read raw microphone data
+        rawsamps = stream.read(1024, exception_on_overflow=False)
+
+        # Convert raw data to NumPy array
+        samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+
+        loudness = analyse.loudness(samps)
+
+        # Show the volume and pitch
+        print loudness
+
+    if loudness > -7:
+        stream.stop_stream()
+        shush()
+        stream.start_stream()
