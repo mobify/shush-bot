32d613af2224d775baeec902ec63454da250eb88
fractaltheory <michael@mobify.com>
2016-01-13 13:10:45 -0800

    More template fields

--- client/webapp.js
+++ client/webapp.js
@@ -10,11 +10,25 @@
         var $template = $('#bots-template');
         var $list = $('#bots');
 
+        var READ_ONLY = [
+            'timestamp'
+        ];
+
         var renderRow = function(data) {
             var $clone = $($template[0].content.querySelectorAll('li')).clone();
-            var $name = $clone.find('.header');
+
+            var $name = $clone.find('.bot-name');
+            var $info = $clone.find('.info');
 
             $name.text(data.name);
+            $info.find('.type').text(data.type);
+            $info.find('.data').text(data.data);
+
+            if (READ_ONLY.indexOf(data.type) !== -1) {
+                $info.find('.data').attr('readonly', 'readonly');
+                $info.find('.submit').attr('disabled', 'disabled');
+            }
+
             $list.append($clone);
         };
 
@@ -29,10 +43,12 @@
                     var mock = {
                         bots: [
                             {
-                                name: 'hello'
+                                name: 'bot1',
+                                type: 'timestamp',
+                                info: '13135823058'
                             },
                             {
-                                name: 'world'
+                                name: 'bot2'
                             }
                         ]
                     };
